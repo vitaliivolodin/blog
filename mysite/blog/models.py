bab9d2ab1b2c9a69691458a102faeb197e8f3bab
Vitalii <vitaliivolodin17@gmail.com>
2017-10-16 23:04:22 +0300

    Fix models.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=255)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    create_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
